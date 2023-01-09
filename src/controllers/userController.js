8229a26f8363b8ce0874f5b68933e54ecdad3d3b
SHOMANS <mohammedshoman9@gmail.com>
2023-01-09 18:10:31 +0200

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
+  if (user && (await user.password) === password) {
     res.json({
       _id: user._id,
       name: user.name,
